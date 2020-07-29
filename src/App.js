ea93c5e33a341d681e6fd50d1ee326d9c2ef39d9
Willyan Fontana <willyanfontana@gmail.com>
2020-07-29 18:06:27 -0300

    Adicionado os arquivos de estilos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { MdAddCircle } from 'react-icons/md'
 
 function App() {
   return (
     <div className="App">
-      
+      sadasdasdas
+      <MdAddCircle className="svg-icon" />
     </div>
   );
 }
