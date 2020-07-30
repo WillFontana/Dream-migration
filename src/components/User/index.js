2d2fa6f33ff6c6a895454deac1013370d15a06ef
WillFontana <willyanfontana@gmail.com>
2020-07-29 23:58:24 -0300

    Adicionado o fetch de usuario

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,30 +1,60 @@
-import React from 'react';
-import UserPic from '../../assets/user.svg'
+import React, { useState, useEffect } from 'react';
+import UserPic from '../../assets/user.svg';
+import api from '../../services/api';
+
 export default function User() {
+
+  const [username, setUserName] = useState('');
+  const [userpicture, setUserPicture] = useState('');
+  const [userjob, setUserJob] = useState('');
+  const [usercompany, setUserCompany] = useState('');
+  const [userdescription, setUserDescription] = useState('');
+  const [userurl, setUserUrl] = useState('');
+
+  async function fetchData() {
+    try {
+      const { data } = await api.get('users/willfontana');
+      setUserName(data.name)
+      setUserPicture(data.avatar_url)
+      setUserJob(data.job)
+      setUserCompany(data.company)
+      setUserDescription(data.bio)
+      setUserUrl(data.html_url)
+    } catch (error) {
+      console.warn(error);
+    }
+
+  }
+
+  fetchData();
+
   return (
     <section className="profile-card">
       <div className="card-header">
-        <img src={UserPic} alt="" className="image-fluid" />
+        <img src={userpicture} alt="" className="image-fluid" />
         <div className="user-name">
           <p className="typo-sub-heading typo-color-dark-secondary typo-informal typo-fw-bold">
-            Willyan Fontana do Prado
+            {username ? username : `Nome desconhecido`}
           </p>
         </div>
       </div>
       <div className="user-descript">
         <p className="typo-body-2 typo-color-dark-secondary typo-formal typo-fw-bold">
-          Trabalho: Front End
+          {userjob ? `Trabalho: ${userjob}` : `Trabalho: Não informado`}
         </p>
         <p className="typo-body-1 typo-color-dark-secondary typo-formal typo-fw-regular">
-          Empresa: Agencia Dream
+          {usercompany ? `Empresa: ${usercompany}` : `Empresa: Não informado`}
         </p>
         <p className="typo-body-1 typo-color-dark-secondary typo-formal typo-fw-regular">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas vitae animi necessitatibus magni iure tempora repudiandae voluptates? Autem magni mollitia, laborum, odio sit ducimus voluptate reiciendis quaerat rerum suscipit vitae.
+          {userdescription ? userdescription : `Sem informações do perfil`}
         </p>
       </div>
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer" className="outline-link">
-        Visitar perfil
-      </a>
+      {userurl &&
+        <a href={userurl} target="_blank" rel="noopener noreferrer" className="outline-link">
+          Visitar perfil
+        </a>
+      }
+
     </section>
   );
 }
