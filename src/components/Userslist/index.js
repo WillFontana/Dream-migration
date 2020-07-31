48e48db96636f5fe7060c25ca337b90e64f54de2
WillFontana <willyanfontana@gmail.com>
2020-07-30 21:44:07 -0300

    Active no item da lista focado

--- src/components/Userslist/index.js
+++ src/components/Userslist/index.js
@@ -15,6 +15,7 @@ function Userslist() {
 
   const [error, setError] = useState(false);
 
+  const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
     setLoading(true);
@@ -30,6 +31,7 @@ function Userslist() {
         setError(true);
       });
   }, []);
+
 
   return (
     <div className="userlist">
@@ -48,20 +50,11 @@ function Userslist() {
           </p>
         </div></>}
       {users.map(user => (
-        <Link key={user.cdusuario} onClick={e => e.target.classList.add = "-active"} to={`/usuarios/${user.cdusuario}`} className="list-item">
-
+        <Link key={user.cdusuario}
+          id={`user-${user.cdusuario}`}
+          to={`/usuarios/${user.cdusuario}`} onClick={e => setCurrentUser(user.cdusuario)}
+          className={`list-item  ${user.cdusuario === currentUser && '-active'}`}>
           <img src={user.txfoto1} onError={(e) => user.txsexo === "masculino" ? e.target.src = MaleDummy : e.target.src = FemaleDummy} alt={user.txnome} className="list-image" />
-
-          {/* <img src={user.txfoto1 || user.txsexo === "masculino" ? MaleDummy : FemaleDummy} alt={user.txnome} className="list-image" />
-
-          {
-            user.txfoto1
-            ?
-            <img src={user.txfoto1} alt={user.txnome} className="list-image" />
-            :
-            <img src={user.txsexo === "masculino" ? MaleDummy : FemaleDummy} alt={user.txnome} className="list-image" />
-          } */}
-
           <p className="typo-body-2 typo-formal typo-color-dark-secondary">
             {user.txnome}
           </p>
